fe375df254dda8c1a592f2b6d4c9c2f8b1878332
kiran <gautamkiran6543@gmail.com>
2026-08-23 00:30:05 +0545

    using final variables and static method in inheritance where we can call directly using class name.method name

--- src/inheritanceexample.java
+++ src/inheritanceexample.java
@@ -1,28 +1,43 @@
+
+//using static in method and final variables in inheritance
+//if we use static method then we can call directly using class name . method name without of creating object
 public class inheritanceexample {
+    static final int age=15;
+    static final String name="Dog";
     public class animal {
-        void feature() {
-            System.out.println("includes all feature");
+
+       static void feature() {
+            System.out.println("name="+name);
+            System.out.println("age="+age);
+           System.out.println(" there are many dog in nepal");
+
         }
     }
-
+static final int weight=30;
+    static final String speed="57 km";
     public class dog extends animal {
-        void run() {
+        static void run() {
+            System.out.println("weight="+weight);
+            System.out.println("speed="+speed);
             System.out.println("it can bark");
         }
     }
-
+static final String animal="cat";
+    static final int ages=2;
     public class cat extends dog {
-        void meow() {
+        static void meow() {
+            System.out.println("Animal name="+animal);
+            System.out.println("Age="+ages);
             System.out.println("it can meow");
         }
     }
+    public static void main(String[] args) { // Here i havent creating any object name i directly
+        // call it using class name and method name (cat.run) in inheritance
+        cat.meow();
+        System.out.println(" ");
+        cat.feature();
+        cat.run();
 
-    public static void main(String[] args) {
-        inheritanceexample obj=new inheritanceexample();
-        cat c= obj.new cat();
-        c.meow();
-        c.run();
-        c.feature();
     }
 }
 
