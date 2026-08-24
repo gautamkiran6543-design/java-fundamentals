992f6e3e58d2674f159aac67a225132abdaf1df9
kiran <gautamkiran6543@gmail.com>
2026-08-24 21:01:51 +0545

    banking system

---
+++ oop/bankingsystem.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+public class bankingsystem {
+    private String bankholder;
+    private double bankbalance;
+    private String accountnumber;
+
+    //setter
+    public void showdetails(String bankholder, double bankbalance, String accountnumber) {
+        this.bankholder = bankholder;
+        this.bankbalance = bankbalance;
+        this.accountnumber = accountnumber;
+    }
+
+    //getter
+    public String getbankholder() {
+        return bankholder;
+    }
+
+    public double getbankbalance() {
+        return bankbalance;
+    }
+
+    public String accountnumber() {
+        return accountnumber;
+    }
+
+    // amount deposited
+    public void deposit(double amount) {
+        if (amount > 0) {
+            bankbalance = bankbalance + amount;
+            System.out.println("Successfully Deposited");
+            System.out.println("Deposited Amount:" + amount);
+            System.out.println("Current balance:" + bankbalance);
+        } else {
+            System.out.println("Invalid Deposited amount");
+        }
+    }
+
+    //amount withdraw
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid withdraw");
+        } else if (amount > bankbalance) {
+            System.out.println("Insufficient bankbalance");
+            System.out.println("current bankbalance:" + bankbalance);
+        } else {
+            bankbalance = bankbalance - amount;
+            System.out.println("withdraw successfully");
+            System.out.println("Withdraw amount:" + amount);
+            System.out.println("Current balance:" + bankbalance);
+
+        }
+    }
+
+    public void showdetails() {
+        System.out.println("-----------------");
+        System.out.println("Account holder name:" + bankholder);
+        System.out.println("Account number:" + accountnumber);
+        System.out.println("Bankbalance:" + bankbalance);
+        System.out.println("------------------");
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a bankholder name:");
+        String name = sc.nextLine();
+        System.out.println("Enter a account number:");
+        String accountnumber = sc.nextLine();
+        System.out.println("Enter a initial bank balance:");
+        double bankbalance = sc.nextDouble();
+        bankingsystem obj = new bankingsystem();
+        int choice;
+        System.out.println("--------WELCOMING TO BANKING SYSTEM----------");
+        System.out.println("1.Deposit");
+        System.out.println("2.With Draw");
+        System.out.println("3.Check Balance");
+        System.out.println("4.Account Details");
+        System.out.println("5.Exit");
+        System.out.println("Enter a choice(1-5)");
+        choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Enter a deposit amount");
+                double depositamount = sc.nextDouble();
+                obj.deposit(bankbalance);
+                break;
+            case 2:
+                System.out.println("Enter a withdraw amount");
+                double amount = sc.nextDouble();
+                obj.withdraw(amount);
+                break;
+            case 3:
+                System.out.println(" Check balance:" + obj.getbankbalance());
+                break;
+            case 4:
+                System.out.println("Account details:");
+                obj.showdetails();
+                break;
+            case 5:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+
+
+        }
+    }
+}
+
+
+
+
