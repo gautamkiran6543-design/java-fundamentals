a4c6596360a43df177793d48c9561b76d1b1bfe9
kiran <gautamkiran6543@gmail.com>
2026-08-25 23:13:49 +0545

    polymorphism

--- oop/polymorphismpayment.java
+++ oop/polymorphismpayment.java
@@ -1,10 +1,12 @@
+//polymorphsim : it mean one name many form where it behave differently in different situations payments method is
+// same way using same method nut different forms like esewa,khalti.
 public class polymorphismpayment {
     void pay() {
         System.out.println("Making payments method using differnts forms");
     }
 
     static class esewa extends polymorphismpayment {
-        @Override
+        @Override // it is used to extends method or replacing/redefining method that is already in parent class
         void pay() {
             System.out.println("Payments using eswa");
         }
@@ -26,7 +28,7 @@ public class polymorphismpayment {
     }
 
     public static void main(String[] args) {
-        polymorphismpayment o1;
+        polymorphismpayment o1; // here i create one refrence can work diferent object
         o1=new polymorphismpayment();
         o1.pay();
         o1 = new esewa();
