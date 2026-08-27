2677572a2e46e5b34ea8aa56104867b6648c31f4
kiran <gautamkiran6543@gmail.com>
2026-08-27 11:28:47 +0545

    inheritance

--- oop/inherit.java
+++ oop/inherit.java
@@ -1,6 +1,6 @@
  public class inherit{
-String parentclass(String name) {
-    return name;
+String parentclass(String name) {  // declaration
+    return name; //defination
 }
 int parentclass(int age){
    return age;
@@ -17,6 +17,8 @@ public class child extends inherit {
 public  void main(String[]args){// i didnot usw public
     child o1=new child(); // i create a object of child class
     o1.parentclass("Dog"); // calling inherit method
+    System.out.println("name:"+ o1.parentclass("Dog"));
     o1.parentclass(25);
+    System.out.println("age:"+ o1.parentclass(23));// this is also method call
     o1.childclass();
 }
