4ba4d3380e3a1e99364f0763795f1af6efc16897
kiran <gautamkiran6543@gmail.com>
2026-09-01 20:28:58 +0545

    recursive

--- oop/javarecursion.java
+++ oop/javarecursion.java
@@ -1,15 +1,29 @@
+//Recursion:it means method calls repeatly until the condition called the base case is reached.
+//void method() {
+//
+//    if (condition) {
+//        return;          // base case
+//    }
+//
+//    method();            // recursive call
+//}
+
 public class javarecursion {
-    static void count(int number) {
+     static void count(int number) { // here we use static method
         if (number == 10) {
             return;
         }
         System.out.println(+number);
 
+count(number+1);
 
-    count(number+1);
 }
 
     public void main(String[] args) {
-        count(1);
+        count(1); // recursive method directly call without object
     }
 }
+//vs loop
+//for(int i=1;i<=10;i++){
+    //System.out.println(i);
+      //  }
