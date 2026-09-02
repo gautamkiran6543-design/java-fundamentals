d258cd5e40374aaf654e8092751201258ed07eca
kiran <gautamkiran6543@gmail.com>
2026-09-02 09:06:01 +0545

    project

---
+++ project/schoolmanagmentsystem.java
@@ -0,0 +1,19 @@
+public class schoolmanagmentsystem {
+    public String name;
+    private int Roll_no;
+    private int age;
+    public String address;
+    public String level;
+
+    public static void main(String[]args){
+        System.out.println("===SCHOOL MANAGEMENT SYSTEM===");
+        System.out.println("1. Add student");
+        System.out.println("2.View Students");
+        System.out.println("3.Search student");
+        System.out.println("4.Update student");
+        System.out.println("5.Delet student");
+        System.out.println("6.Add Teacher");
+        System.out.println("7.View teacher");
+        System.out.println("8.Exit");
+    }
+}
