dfc08d457d588bb4bd60fcf03c2f5387a5cabed7
kiran <gautamkiran6543@gmail.com>
2026-09-04 00:09:41 +0545

    arraylist to store data

--- oop/arraylist.java
+++ oop/arraylist.java
@@ -4,14 +4,21 @@ public class arraylist {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer>age= new ArrayList<>();
+        ArrayList<Integer> age = new ArrayList<>();
         System.out.println("How many student do you want to store");
-        input.nextInt();
-        System.out.println("Enter a name");
-        String name=input.next();
-        names.add(name);
-        System.out.println("Enter a age");
-        int ages=input.nextInt();
-        age.add(ages);
+        int n=input.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter a name");
+            String name = input.next();
+            names.add(name);
+            System.out.println("Enter a age");
+            int ages = input.nextInt();
+            age.add(ages);
+        }
+        System.out.println("stored detail successfully:");
+        for(int i=0;i<names.size();i++){
+            System.out.println("Name="+names.get(i)+
+            "\nAge="+age.get(i));
+        }
     }
 }
