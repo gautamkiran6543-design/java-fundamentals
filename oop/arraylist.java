51f472986b7377b7f58eb1c24844a6dd1105a28a
kiran <gautamkiran6543@gmail.com>
2026-09-03 23:58:37 +0545

    arraylist syoring

--- oop/arraylist.java
+++ oop/arraylist.java
@@ -3,18 +3,15 @@ import java.util.ArrayList;
 public class arraylist {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Integer> num = new ArrayList<>();
-        num.add(11);
-        num.add(15);
-        ArrayList<String> name = new ArrayList<>();
-        name.add("ram");
-        name.add("kiran");
-        name.add("Kg");
-        for(int i=0;i<name.size();i++){
-            System.out.println(name.get(i));
-            System.out.println(num.get(i));
-
-        }
-
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Integer>age= new ArrayList<>();
+        System.out.println("How many student do you want to store");
+        input.nextInt();
+        System.out.println("Enter a name");
+        String name=input.next();
+        names.add(name);
+        System.out.println("Enter a age");
+        int ages=input.nextInt();
+        age.add(ages);
     }
 }
