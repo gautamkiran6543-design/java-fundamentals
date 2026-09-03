ad113f4789550b1d64d27fb36b5f65f9f593f6e5
kiran <gautamkiran6543@gmail.com>
2026-09-03 22:36:04 +0545

    arraylist

--- oop/arraylist.java
+++ oop/arraylist.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class arraylist {
-    public static void main(String[]args){
-        Scanner input= new Scanner(System.in);
-        ArrayList<String> names= new ArrayList<>();
-        String name;
-        System.out.println("Enter a name");
-        names.add("ram");
-        names.add("kiran");
-        names.add("gautam");
-        System.out.println(names);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(11);
+        num.add(15);
+        ArrayList<String> name = new ArrayList<>();
+        name.add("ram");
+        name.add("kiran");
+        name.add("Kg");
+        for(int i=0;i<name.size();i++){
+            System.out.println(name.get(i));
+            System.out.println(num.get(i));
+
+        }
+
     }
 }
