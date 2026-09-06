8749bf3d44e09ffca090d082c3a3b63145b24df0
kiran <gautamkiran6543@gmail.com>
2026-09-06 22:49:36 +0545

    local time

--- oop/localdate.java
+++ oop/localdate.java
@@ -1,8 +1,12 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class localdate {
     public void main(String[]args){
         LocalDate today=LocalDate.now();
         System.out.println(today);
+        LocalTime now=LocalTime.now();
+        System.out.println(now);
     }
 
 }
