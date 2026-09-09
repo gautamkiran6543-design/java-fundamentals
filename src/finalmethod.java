d2347242fae032c2c8d7c3bfa9a4d66e51793283
kiran <gautamkiran6543@gmail.com>
2026-09-09 23:53:31 +0545

    cannot override final method

--- src/finalmethod.java
+++ src/finalmethod.java
@@ -1,10 +1,21 @@
-public class finalmethod {
+ public class finalmethod {
     final String name="kiran";
     final int age=5;
     final static void detail(String name,int age){
         System.out.println("name="+name);
         System.out.println("age="+age);
     }
+    class car extends finalmethod{
+        @Override
+        void detail(String name, int age){
+            //compile error
+            //cannot override a final method
+            //error
+
+        }
+
+    }
+
     public void main(String[]args){
         finalmethod.detail("sauj",21);
 
