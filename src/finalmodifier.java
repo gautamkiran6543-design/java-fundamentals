2734bc51fd5ae2f07f1046a38d4bdfc5c845e19d
kiran <gautamkiran6543@gmail.com>
2026-09-09 23:24:04 +0545

    modifier

--- src/finalmodifier.java
+++ src/finalmodifier.java
@@ -1,7 +1,8 @@
 public class finalmodifier {
-    public static void main(String[] args){
-       final int a=10;
-        a=45;
+    final int a=10;
+    final static String name="kiran";
+    public  void main(String[] args){
         System.out.println(a);
+        System.out.println(name);
     }
 }
