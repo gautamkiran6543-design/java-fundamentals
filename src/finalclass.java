9ff91c90451a8fd3a15b84c73730bc9f1455120a
kiran <gautamkiran6543@gmail.com>
2026-09-10 09:12:57 +0545

    modifier

--- src/finalclass.java
+++ src/finalclass.java
@@ -1,12 +1,20 @@
 public final class finalclass {//it means it cannot be inherited by another class
-    void detail(final int age){
-        System.out.println("Age="+age);
+
+    void detail(final int age) {
+        System.out.println("Age=" + age);
     }
-    public void main(String []args){
-        finalclass o=new finalclass();
-        o.detail(89);
+
+    //class car extends finalclass {
+        void detail()
+        {
+            System.out.println("it cannot br inherited due to final modifier");
+        }        public void main(String[] args) {
+            finalclass o = new finalclass();
+            o.detail(89);
+            o.detail();
 
         }
     }
+//}
 
 
