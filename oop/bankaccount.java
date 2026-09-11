431dffcd8795d9f325b6b1a04fa2bf96cb4ec6e5
kiran <gautamkiran6543@gmail.com>
2026-09-12 00:02:02 +0545

    banking account

--- oop/bankaccount.java
+++ oop/bankaccount.java
@@ -1,11 +1,21 @@
 abstract  class bankaccount {//abstract class
     double balance;
+    // ABSTRACTION:
+    // We only define WHAT a bank account must do.
+    // We do NOT tell here HOW deposit will work.
+    //Every account must have withdraw(),
+    // but the actual withdrawal rules are hidden
+    // and will be implemented by child classes.
     abstract void deposit(double amount);//abstract method
     abstract void withdraw(double amount);
+    // NORMAL METHOD:
+    // Common behavior that all accounts can use.
     void showbalance(){
         System.out.println("Balance:" +balance);
     }
 }
+// HOW deposit works for SavingAccount is defined here.
+// This implementation was hidden from BankAccount.
 class savingaccount extends bankaccount{
     void deposit(double amount){
         balance=balance+amount;
@@ -13,6 +23,7 @@ class savingaccount extends bankaccount{
         System.out.println("Deposited successfully");
 
     }
+    // HOW withdrawal works for SavingAccount is defined here.
     void withdraw(double amount){
         if(balance>=amount){
             balance=balance-amount;
@@ -24,12 +35,16 @@ class savingaccount extends bankaccount{
     }
 
 }
+
 class currentaccount extends bankaccount{
+    // HOW deposit works for CurrentAccount.
     void deposit(double amount){
         balance=balance+amount;
         System.out.println("Deposit:"+amount);
         System.out.println("Deposited successfully!");
     }
+    // CurrentAccount has a DIFFERENT withdrawal rule.
+    // It allows an additional 10,000 overdraft.
     void withdraw(double amount){
         if(amount<=balance+10000){
             balance=balance-amount;
