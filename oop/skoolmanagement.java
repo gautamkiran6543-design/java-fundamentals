7666c6bdf2440ef0857056937949ffa6b8512dc7
kiran <gautamkiran6543@gmail.com>
2026-09-12 23:25:13 +0545

    uptading students record

--- oop/skoolmanagement.java
+++ oop/skoolmanagement.java
@@ -18,21 +18,26 @@ public class skoolmanagement {
 public  void main(String[]args){
     ArrayList<skoolmanagement> student=new ArrayList<>();
     Scanner input=new Scanner(System.in);
-    System.out.println("Enter a student ID:");
-    int id=input.nextInt();
-    System.out.println("Enter a student Name:");
-    String name=input.next();
-    System.out.println("Enter a student address:");
-    String address= input.next();
-    System.out.println("Enter a student level:");
-    String level=input.next();
-    skoolmanagement s=new skoolmanagement(id,name,address,level); // here i create an object
+    System.out.println("--------------------------------------------------------");
+    System.out.println("====How many students records do you want to store?====");
+    int n= input.nextInt();
 
-    //add oject to arraylist
-    student.add(s);
-    System.out.println("Records are added successfully ✌️✌️");
-    System.out.println("-----------------------------------");
+    for(int i=0;i<n;i++) {
+        System.out.println("Enter a student ID:");
+        int id = input.nextInt();
+        System.out.println("Enter a student Name:");
+        String name = input.next();
+        System.out.println("Enter a student address:");
+        String address = input.next();
+        System.out.println("Enter a student level:");
+        String level = input.next();
+        skoolmanagement s = new skoolmanagement(id, name, address, level); // here i create an object
 
+        //add oject to arraylist
+        student.add(s);
+        System.out.println("Records are added successfully ✌️✌️");
+        System.out.println("-----------------------------------");
+    }
 
     //this part is for storing data
     for(int i=0;i<student.size();i++){
@@ -43,5 +48,26 @@ public  void main(String[]args){
         System.out.println("Level:"+s1.level);
     }
     System.out.println("---------------------------------");
+
+    System.out.println("-------Enter a student id to update Data?-------");
+    int searchId=input.nextInt();
+    for (int i=0;i< student.size();i++){
+        skoolmanagement s1=student.get(i);
+        if(s1.id==searchId){ // Check whether the current student's ID matches the ID we entered
+            //new records are generate
+            System.out.println("Enter a new name:");
+            String name=input.next();
+            System.out.println("Enter a new address:");
+            String address=input.next();
+            System.out.println("Enter a new level:");
+            String level=input.next();
+            System.out.println("---------successfully new records are Updated---------");
+            break; //stop the loop because we found the id
+
+        }
+        else{
+            System.out.println("--------------Invalid ID----------------");
+        }
+    }
 
 }
