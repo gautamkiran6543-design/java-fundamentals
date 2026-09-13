8c280e23c6ec6e0a5c4373ced07ec35f1d392e79
kiran <gautamkiran6543@gmail.com>
2026-09-13 20:27:02 +0545

    complete the project

--- oop/skoolmanagement.java
+++ oop/skoolmanagement.java
@@ -33,14 +33,15 @@ class teacher{
 }
 public  void main(String[]args) {
     ArrayList<skoolmanagement> student = new ArrayList<>();
+    ArrayList<teacher> teacher = new ArrayList<>();
     Scanner input = new Scanner(System.in);
 
     boolean flag = true;
     while (flag) {
         System.out.println("🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶");
         System.out.println("===SCHOOL MANAGEMENT SYSTEM===");
-        System.out.println("   1.STUDENT");
-        System.out.println("   2.TEACHER");
+        System.out.println("   1.MANAGE STUDENT");
+        System.out.println("   2.MANAGE TEACHER");
         System.out.println("   3.EXIT");
         System.out.println("   Enter a Choice (1-3)");
         System.out.println("🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶");
@@ -55,7 +56,6 @@ public  void main(String[]args) {
                     System.out.println("   2.View Students");
                     System.out.println("   3.Update student");
                     System.out.println("   4.Delet student");
-                    System.out.println("   5.Add Teacher");
                     System.out.println("   5.Exit");
                     System.out.println("   Enter a choice(1-5)");
                     System.out.println("🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶");
@@ -90,11 +90,13 @@ public  void main(String[]args) {
                             //this part is for storing data
                             for (int i = 0; i < student.size(); i++) {
                                 skoolmanagement s1 = student.get(i); // Get the student object from the ArrayList at index i
+                                System.out.println("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁");
                                 System.out.println("ID:" + s1.id);
                                 System.out.println("Name:" + s1.name);
                                 System.out.println("Address:" + s1.address);
                                 System.out.println("Level:" + s1.level);
                                 System.out.println("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁");
+                                System.out.println();
                             }
                             System.out.println("-------------------------------------");
                             break;
@@ -154,7 +156,122 @@ public  void main(String[]args) {
                             break;
                     }
                 }
+
+                //Teacher
+            case 2:
+                while (flag) {
+                    System.out.println("🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶");
+                    System.out.println("===Welcome to Teacher Management System===");
+                    System.out.println("   1. Add Teachers");
+                    System.out.println("   2.View Teachers");
+                    System.out.println("   3.Update Teachers");
+                    System.out.println("   4.Delet Teachers");
+                    System.out.println("   5.Exit");
+                    System.out.println("   Enter a choice(1-5)");
+                    System.out.println("🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶🪶");
+                    int teacherchoice = input.nextInt();
+                    switch (teacherchoice) {
+
+                        //add
+                        case 1:
+                            System.out.println("--------------------------------------------------------");
+                            System.out.println("====How many Teachers records do you want to store?====");
+                            int n = input.nextInt();
+
+                            for (int i = 0; i < n; i++) {
+                                System.out.println("Enter a Teacher ID:");
+                                int id = input.nextInt();
+                                System.out.println("Enter a Teacher Name:");
+                                String name = input.next();
+                                System.out.println("Enter a Teacher address:");
+                                String address = input.next();
+                                System.out.println("Enter a Teacher subject:");
+                                String subject = input.next();
+                                teacher t = new teacher(id, name, address, subject); // here i create an object
+
+                                //add oject to arraylist
+                                teacher.add(t);
+                                System.out.println("Records are added successfully ✌️✌️");
+                                System.out.println("-----------------------------------");
+                            }
+                            break;
+
+                        //view
+                        case 2:
+                            for (int i = 0; i < teacher.size();
+                                 i++) {
+                                teacher t=teacher.get(i);
+                                System.out.println("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁");
+                                System.out.println("ID:"+t.id);
+                                System.out.println("Name:"+t.name);
+                                System.out.println("Address:"+t.address);
+                                System.out.println("Subject:"+t.subject);
+                                System.out.println("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁");
+                                System.out.println();
+
+
+                            }
+                            System.out.println("-------------------------------------");
+                            break;
+
+                            //update
+                        case 3:
+                            System.out.println("--------------Enter a Teacher id to update-------------");
+                            int searchid=input.nextInt();
+                            for(int i=0;i<teacher.size();i++){
+                                teacher t=teacher.get(i);
+                                if(t.id==searchid){
+                                    System.out.println("Enter a new name");
+                                    String name=input.next();
+                                    System.out.println("Enter a new address");
+                                    String address=input.next();
+                                    System.out.println("Enter a new subject name");
+                                    String subject=input.next();
+
+                                    //store data
+                                    t.name=name;
+                                    t.address=address;
+                                    t.subject=subject;
+                                    break;
+
+                                }
+                                else{
+                                    System.out.println("------Invalid ID-------");
+                                }
+
+                            }
+                            break;
+
+                            //Delet
+                        case 4:
+                            System.out.println("----Enter a ID of teacher to Delet---");
+                             searchid=input.nextInt();
+                             for(int i=0; i<teacher.size();i++){
+                                 teacher t=teacher.get(i);
+                                 if(t.id==searchid){
+                                     teacher.remove(i);
+                                     System.out.println("Successfully Deleted the records");
+                                     break;
+                                 }
+                                 else{
+                                     System.out.println("------Invalid ID------");
+                                 }
+
+
+                             }
+                             break;
+                        case 5:
+                            System.out.println("Exit");
+                            flag=false;
+                            break;
+                    }
+                }
+
+            case 3:
+                System.out.println("Exit");
+                flag=false;
+                break;
+
         }
     }
 }
-
