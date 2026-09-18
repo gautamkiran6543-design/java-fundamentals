23170f84cab4bea96d1e2d6390f158151a7e3658
kiran <gautamkiran6543@gmail.com>
2026-09-18 23:20:58 +0545

    Exception handaling

---
+++ .idea/advance/exception.java
@@ -0,0 +1,14 @@
+public class exception {
+    public static void main(String[]args){
+    int a=10;
+    int b=0;
+    try {
+        int result = a / b;
+        System.out.println(result);
+    }
+    catch(ArithmeticException e){
+        System.out.println("it executes the program");
+        }
+
+}
+}
