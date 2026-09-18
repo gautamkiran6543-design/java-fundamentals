8cc573acc50692d283cb491a592c2e9e8fb960a6
kiran <gautamkiran6543@gmail.com>
2026-09-18 23:47:35 +0545

    exception

--- .idea/advance/exception.java
+++ .idea/advance/exception.java
@@ -1,14 +1,19 @@
 public class exception {
-    public static void main(String[]args){
-    int a=10;
-    int b=0;
-    try {
-        int result = a / b;
-        System.out.println(result);
-    }
-    catch(ArithmeticException e){
-        System.out.println("it executes the program");
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try { //Contains risky code
+            int result = a / b;
+            System.out.println(result);
         }
-
-}
+        catch (ArithmeticException e) { //handles exception
+            System.out.println("it executes the program");
+        }
+        finally { //always runs
+            System.out.println("it runs the exception always");
+        }
+        if(0>10){
+            throw new ArithmeticException("mistake");
+        }
+    }
 }
