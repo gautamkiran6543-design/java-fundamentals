232ea5de16083a4be503c3ebf1c84263529748a5
kiran <gautamkiran6543@gmail.com>
2026-09-26 17:43:28 +0545

    super keyword in parent variables method and constructor

--- oop/parent.java
+++ oop/parent.java
@@ -1,15 +1,27 @@
 public class parent {
     String name="kiran";
+    parent(){ //parent class constructor
+        System.out.println("This is parent class constructor");
+    }
+    void show(String address, int age){
+        System.out.println("Address:"+address);
+        System.out.println("Age:"+age);
+    }
 }
 class child extends parent{
     String name="SG";
-    void display(){
-        System.out.println(name);
-        System.out.println(super.name);
+    child(){ // this is child class constructor
+        System.out.println("This is child class constructor");
+        super(); //call parent class constructor
     }
+    void display(){
+        System.out.println("Child name:" +name);
+        System.out.println("Parent class name:" +super.name);
+        super.show("itahari",21);
+    }
+
 }
 public void main(String[]args){
     child c= new child();
-    System.out.println(c.name);
     c.display();
 }
